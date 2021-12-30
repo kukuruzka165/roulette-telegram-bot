3ceb0497869aa85851ade392e04bc716f368f7ad
KUKURUZKA <60149954+KUKURUZKA165@users.noreply.github.com>
2021-12-30 15:32:46 +0200

    no more trash code in main.py!

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import asyncio
 from config import TOKEN  # , gif_id
 from aiogram import Bot, Dispatcher, executor, types
 import requests
+from algorithm import *
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
@@ -71,119 +72,7 @@ async def roll(message):
     rnd = randomorg_parse(37)
     await log(f"{gamecode} - Выпал вариант {rnd} из 37.")
 
-    #  elif? Нет, не учили)
-
-    result = "Ошибка. Вероятнее всего проблема на стороне random.org"
-    if rnd == 1:
-        result = "Зеро! 🟢 Зелёный"
-    else:
-        if rnd == 2:
-            result = "32, 🔴 Красный"
-        else:
-            if rnd == 3:
-                result = "15, ⚫ Чёрный"
-            else:
-                if rnd == 4:
-                    result = "19, 🔴 Красный"
-                else:
-                    if rnd == 5:
-                        result = "4, ⚫ Чёрный"
-                    else:
-                        if rnd == 6:
-                            result = "21, 🔴 Красный"
-                        else:
-                            if rnd == 7:
-                                result = "2, ⚫ Чёрный"
-                            else:
-                                if rnd == 8:
-                                    result = "25, 🔴 Красный"
-                                else:
-                                    if rnd == 9:
-                                        result = "17, ⚫ Чёрный"
-                                    else:
-                                        if rnd == 10:
-                                            result = "34, 🔴 Красный"
-                                        else:
-                                            if rnd == 11:
-                                                result = "6, ⚫ Чёрный"
-                                            else:
-                                                if rnd == 12:
-                                                    result = "27, 🔴 Красный"
-                                                else:
-                                                    if rnd == 13:
-                                                        result = "13, ⚫ Чёрный"
-                                                    else:
-                                                        if rnd == 14:
-                                                            result = "36, 🔴 Красный"
-                                                        else:
-                                                            if rnd == 15:
-                                                                result = "11, ⚫ Чёрный"
-                                                            else:
-                                                                if rnd == 16:
-                                                                    result = "30, 🔴 Красный"
-                                                                else:
-                                                                    if rnd == 17:
-                                                                        result = "8, ⚫ Чёрный"
-                                                                    else:
-                                                                        if rnd == 18:
-                                                                            result = "23, 🔴 Красный"
-                                                                        else:
-                                                                            if rnd == 19:
-                                                                                result = "10, ⚫ Чёрный"
-                                                                            else:
-                                                                                if rnd == 20:
-                                                                                    result = "5, 🔴 Красный"
-                                                                                else:
-                                                                                    if rnd == 21:
-                                                                                        result = "24, ⚫ Чёрный"
-                                                                                    else:
-                                                                                        if rnd == 22:
-                                                                                            result = "16, 🔴 Красный"
-                                                                                        else:
-                                                                                            if rnd == 23:
-                                                                                                result = "33, ⚫ Чёрный"
-                                                                                            else:
-                                                                                                if rnd == 24:
-                                                                                                    result = "1, 🔴 Красный"
-                                                                                                else:
-                                                                                                    if rnd == 25:
-                                                                                                        result = "20, ⚫ Чёрный"
-                                                                                                    else:
-                                                                                                        if rnd == 26:
-                                                                                                            result = "14, 🔴 Красный"
-                                                                                                        else:
-                                                                                                            if rnd == 27:
-                                                                                                                result = "31, ⚫ Чёрный"
-                                                                                                            else:
-                                                                                                                if rnd == 28:
-                                                                                                                    result = "9, 🔴 Красный"
-                                                                                                                else:
-                                                                                                                    if rnd == 29:
-                                                                                                                        result = "22, ⚫ Чёрный"
-                                                                                                                    else:
-                                                                                                                        if rnd == 30:
-                                                                                                                            result = "18, 🔴 Красный"
-                                                                                                                        else:
-                                                                                                                            if rnd == 31:
-                                                                                                                                result = "29, ⚫ Чёрный"
-                                                                                                                            else:
-                                                                                                                                if rnd == 32:
-                                                                                                                                    result = "7, 🔴 Красный"
-                                                                                                                                else:
-                                                                                                                                    if rnd == 33:
-                                                                                                                                        result = "28, ⚫ Чёрный"
-                                                                                                                                    else:
-                                                                                                                                        if rnd == 34:
-                                                                                                                                            result = "12, 🔴 Красный"
-                                                                                                                                        else:
-                                                                                                                                            if rnd == 35:
-                                                                                                                                                result = "35, ⚫ Чёрный"
-                                                                                                                                            else:
-                                                                                                                                                if rnd == 36:
-                                                                                                                                                    result = "3, 🔴 Красный"
-                                                                                                                                                else:
-                                                                                                                                                    if rnd == 37:
-                                                                                                                                                        result = "26, ⚫ Чёрный"
+    result = fun_result(rnd)
 
     await log(f"{gamecode} - Результат - {result}.")  # Ждём {rnd_sleep} сек...")
     #  await asyncio.sleep(rnd_sleep)
