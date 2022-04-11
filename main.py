6ba56c1797306d276d5df3bb02ac1fdd4e04e7eb
KUKURUZKA <60149954+KUKURUZKA165@users.noreply.github.com>
2022-04-11 21:18:15 +0300

    Слава Україні!

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
 markup1 = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-markup1.add(types.KeyboardButton("Случайный факт"), types.KeyboardButton("Автор"), types.KeyboardButton("Играть"))
-helpmsg = "<b>Вот список доступных команд:</b>\n\n<b>/roll</b> - Играть в рулетку (Также есть на клавиатуре)\n\n<b>/orlanka</b> - Играть в орлянку (орёл или решка)\n\n<b>/dice</b> - Подбросить кости\n\n<b>/fact</b> - Случаный факт про этого бота (Также есть на клавиатуре)\n\n<b>/disclaimer</b> - Отказ от ответственности\n\n<b>/author</b> - Автор бота (Также есть на клавиатуре)\n\n<b>/ping</b> - Понг!\n\n<b>/keyboard</b> - Открыть заново клавиатуру для игры\n\n<b>/rm_keyboard</b> - Закрыть клавиатуру. Полезно в группах\n\n<b>/help</b> - Показать все команды"
+markup1.add(types.KeyboardButton("Випадковий факт"), types.KeyboardButton("Автор"), types.KeyboardButton("Грати"))
+helpmsg = "<b>Ось список доступних команд:</b>\n\n<b>/roll</b> - Грати в рулетку (Також є на клавіатурі)\n\n<b>/orlanka</b> - Грати в орлянку (орел або решка)\n\n<b>/dice</b> - Підкинути кістки\n\n<b>/fact</b> - Випадковий факт про цього робота (Також є на клавіатурі)\n\n<b>/disclaimer</b> - Відмова від відповідальності\n\n<b>/author</b> - Автор бота (Також є на клавіатурі)\n\n<b>/ping</b> - Понг!\n\n<b>/keyboard</b> - Відкрити заново клавіатуру для гри\n\n<b>/rm_keyboard</b> - Закрити клавіатуру. Корисно у групах\n\n<b>/help</b> - Показати всі команди"
 
 print(r"   ____                   ____              __     __  __            ____        __ ")
 print(r"  / __ \____  ___  ____  / __ \____  __  __/ /__  / /_/ /____       / __ )____  / /_")
@@ -36,33 +36,33 @@ def randomorg_parse(number):
 @dp.message_handler(commands=["start"])
 async def start(message):
     await log(f"--------------------\n{time.ctime()}\n{message.from_user.first_name} {message.from_user.last_name} @{message.from_user.username} id={message.from_user.id}\n{message.chat.title} {message.chat.invite_link} id = {message.chat.id}\n-")
-    await log("Запрошен старт")
+    await log("Запитаний старт")
 
-    send_mess = f"<b>Привет, {message.from_user.first_name}! В этом боте ты сможешь крутить рулетку.</b>"
+    send_mess = f"<b>Привіт, {message.from_user.first_name}! У цьому боті ти зможеш крутити рулетку.</b>"
     await bot.send_message(message.chat.id, send_mess, parse_mode="html")
 
-    send_mess = "<b>Поскольку бот бесплатный, он не хостится на удалённом сервере. Из-за этого я не могу держать его включённым 24/7. Приношу свои извинения!</b>"
+    send_mess = "<b>Оскільки бот безкоштовний, він не хоститься на віддаленому сервері. Через це я не можу тримати його увімкненим 24/7. Приношу свої вибачення!</b>"
     await bot.send_message(message.chat.id, send_mess, parse_mode="html")
 
-    send_mess = "<b>Важно для владельцев групп: </b>\nУ бота ограничен доступ к сообщениям в группах на уровне Telegram API. Он не сможет за вами подглядывать :)\n<b><a href='https://core.telegram.org/bots#privacy-mode'>Подробнее</a></b>"
+    send_mess = "<b>Важливо для власників груп: </b>\nУ роботі обмежений доступ до повідомлень у групах на рівні Telegram API. Він не зможе за вами підглядати :)\n<b><a href='https://core.telegram.org/bots#privacy-mode'>Докладніше</a></b>"
     await bot.send_message(message.chat.id, send_mess, parse_mode="html", disable_web_page_preview=True)
 
-    send_mess = helpmsg + "\n\n\n<b>Или пользуйся клавиатурой с кнопками:</b>"
+    send_mess = helpmsg + "\n\n\n<b>Або користуйся клавіатурою з кнопками:</b>"
     await bot.send_message(message.chat.id, send_mess, parse_mode="html")
 
-    send_mess = "<b>Удачи!</b>"
+    send_mess = "<b>🇺🇦 Слава Україні! 🇺🇦</b>"
     await bot.send_message(message.chat.id, send_mess, parse_mode="html", reply_markup=markup1)
 
-    await log("Старт отправлен!")
+    await log("Старт відправлено!")
 
 
 @dp.message_handler(commands=["roll"])
 async def roll(message):
     await log(f"--------------------\n{time.ctime()}\n{message.from_user.first_name} {message.from_user.last_name} @{message.from_user.username} id={message.from_user.id}\n{message.chat.title} {message.chat.invite_link} id = {message.chat.id}\n-")
-    send_mess = "<b>Ожидание ответа от random.org</b>"
+    send_mess = "<b>Очікування відповіді від random.org</b>"
     roll_mess = await bot.send_message(message.chat.id, send_mess, parse_mode="html", disable_web_page_preview=True)
     gamecode = randint(100, 999)
-    await log(f"{gamecode} - Обращение к random.org...")
+    await log(f"{gamecode} - Звернення до random.org...")
 
     rnd = randomorg_parse(37)
     result = fun_result(rnd)
@@ -74,8 +74,8 @@ async def roll(message):
 @dp.message_handler(commands=["ping"])
 async def ping(message):
     await log(f"--------------------\n{time.ctime()}\n{message.from_user.first_name} {message.from_user.last_name} @{message.from_user.username} id={message.from_user.id}\n{message.chat.title} {message.chat.invite_link} id = {message.chat.id}\n-")
-    await log("Пинг?")
-    send_mess = f"<b>Понг! Я жив!</b>"
+    await log("Пінг?")
+    send_mess = f"<b>Понг! Я живий!</b>"
     await bot.send_message(message.chat.id, send_mess, parse_mode="html")
     await log("Понг!")
 
@@ -83,60 +83,61 @@ async def ping(message):
 @dp.message_handler(commands=["disclaimer"])
 async def disclaimer(message):
     await log(f"--------------------\n{time.ctime()}\n{message.from_user.first_name} {message.from_user.last_name} @{message.from_user.username} id={message.from_user.id}\n{message.chat.title} {message.chat.invite_link} id = {message.chat.id}\n-")
-    await log("Запрошен дисклеймер.")
-    send_mess = "<b>ДИСКЛЕЙМЕР (ОТКАЗ ОТ ОТВЕТСТВЕННОСТИ): </b>Я полностью отказываюсь от результатов использования " \
-                "данного бота не в целях развлечения. Бот создан не в коммерческих целях, и никогда таким не " \
-                "станет!\n\n<b>Коротко:</b> Ставок нет."
+    await log("Запитаний дисклеймер.")
+    send_mess = "<b>ДИСКЛЕЙМЕР (ВІДМОВА ВІД ВІДПОВІДАЛЬНОСТІ): </b>Я повністю відмовляюся від результатів використання " \
+                "даного бота не з метою розваги. Бот створений не з комерційною метою, і ніколи таким не " \
+                "стане!\n\n<b>Коротко:</b> Ставок нема."
     await bot.send_message(message.chat.id, send_mess, parse_mode="html")
-    await log("Дисклеймер отправлен.")
+    await log("Дисклеймер відправлено.")
 
 
 @dp.message_handler(commands=["author"])
 async def author(message):
     await log(f"--------------------\n{time.ctime()}\n{message.from_user.first_name} {message.from_user.last_name} @{message.from_user.username} id={message.from_user.id}\n{message.chat.title} {message.chat.invite_link} id = {message.chat.id}\n-")
-    await log("Запрошен автор.")
-    send_mess = f"<b>🧑🏻‍💻 Мой автор - @anton165\nЕму можно давать идеи для новых функций в боте.</b>"
+    await log("Запрошений автор.")
+    send_mess = f"<b>🧑🏻‍💻 Мій автор - @anton165\nЙому можна давати ідеї для нових функцій у боті.</b>"
     await bot.send_message(message.chat.id, send_mess, parse_mode="html")
-    await log("Автор заслан)")
+    await log("Автор засланий)")
 
 
 @dp.message_handler(commands=["keyboard"])
 async def keyboard(message):
     await log(f"--------------------\n{time.ctime()}\n{message.from_user.first_name} {message.from_user.last_name} @{message.from_user.username} id={message.from_user.id}\n{message.chat.title} {message.chat.invite_link} id = {message.chat.id}\n-")
-    await log("Запрошено открытие клавиатуры!")
-    send_mess = f"<b>Клавиатура открыта!</b>"
+    await log("Запрошено відкриття клавіатури!")
+    send_mess = f"<b>Клавіатура відкрита!</b>"
     await bot.send_message(message.chat.id, send_mess, parse_mode="html", reply_markup=markup1)
-    await log("Клавиатура открыта.")
+    await log("Клавіатура відкрита.")
 
 
 @dp.message_handler(commands=['rm_keyboard'])
 async def rm_keyboard(message):
-    await log("Запрошено закрытие клавиатуры!")
-    send_mess = f"<b>Клавиатура закрыта!</b>"
+    await log(f"--------------------\n{time.ctime()}\n{message.from_user.first_name} {message.from_user.last_name} @{message.from_user.username} id={message.from_user.id}\n{message.chat.title} {message.chat.invite_link} id = {message.chat.id}\n-")
+    await log("Запитано закриття клавіатури!")
+    send_mess = f"<b>Клавіатура закрита!</b>"
     await bot.send_message(message.chat.id, send_mess, parse_mode="html", reply_markup=types.ReplyKeyboardRemove())
-    await log("Клавиатура закрыта.")
+    await log("Клавіатура закрита.")
 
 
 @dp.message_handler(commands=["orlanka"])
 async def orlanka(message):
     await log(f"--------------------\n{time.ctime()}\n{message.from_user.first_name} {message.from_user.last_name} @{message.from_user.username} id={message.from_user.id}\n{message.chat.title} {message.chat.invite_link} id = {message.chat.id}\n-")
     gamecode = randint(10, 99)
-    send_mess = "<b>Ожидание ответа от random.org</b>"
+    send_mess = "<b>Очікування відповіді від random.org</b>"
     oreshka_mess = await bot.send_message(message.chat.id, send_mess, parse_mode="html", disable_web_page_preview=True)
-    await log(f"{gamecode} - Запрошена орлянка.")
+    await log(f"{gamecode} - Запитана орлянка.")
     oreshka = randomorg_parse(2)
 
-    resultoreshka = "Ошибка. Вероятнее всего проблема на стороне random.org"
+    resultoreshka = "Помилка. Скоріше за все проблема на стороні random.org"
     if oreshka == 1:
-        await log(f"{gamecode} - Выпал орёл. Отправляю...")
-        resultoreshka = "Орёл"
+        await log(f"{gamecode} - Випав орел. Відправляю...")
+        resultoreshka = "Орел"
     else:
         if oreshka == 2:
-            await log(f"{gamecode} - Выпала решка. Отправляю...")
+            await log(f"{gamecode} - Випала решка. Відправляю...")
             resultoreshka = "Решка"
 
     await bot.edit_message_text(chat_id=message.chat.id, message_id=oreshka_mess.message_id, text=f"<b>{message.from_user.first_name}, ваш результат:\n{resultoreshka}.</b>", parse_mode="html")
-    await log("Отправлено.")
+    await log("Надіслано.")
 
 
 @dp.message_handler(commands=["dice"])
@@ -144,10 +145,10 @@ async def dice(message):
     await log(f"--------------------\n{time.ctime()}\n{message.from_user.first_name} {message.from_user.last_name} @{message.from_user.username} id={message.from_user.id}\n{message.chat.title} {message.chat.invite_link} id = {message.chat.id}\n-")
     gamecode = randint(10, 99)
     dice_sleep = 3
-    await log(f"{gamecode} - Запрошен дайс.")
+    await log(f"{gamecode} - Запрошений дайс.")
     dice_message = await bot.send_dice(message.chat.id, emoji="🎲")
-    await log(f"{gamecode} - Подкинул. Результат: {dice_message.dice.value}. Ждём {dice_sleep} сек...")
-    send_mess = f"<b>Пожалйста подождите..</b>"
+    await log(f"{gamecode} - Підкинув. Результат: {dice_message.dice.value}. Чекаємо {dice_sleep} сек...")
+    send_mess = f"<b>Зачекайте, будь ласка...</b>"
     dice_comment_message = await bot.send_message(message.chat.id, send_mess, parse_mode="html")
 
     dice_emoji = ""
@@ -177,35 +178,35 @@ async def dice(message):
 @dp.message_handler(commands=["fact"])
 async def fact(message):
     await log(f"--------------------\n{time.ctime()}\n{message.from_user.first_name} {message.from_user.last_name} @{message.from_user.username} id={message.from_user.id}\n{message.chat.title} {message.chat.invite_link} id = {message.chat.id}\n-")
-    await log("Запрошен факт.")
+    await log("Запитаний факт.")
     howmanyfacts = 5
-    send_fact_mess = "Произошла ошибка!"
+    send_fact_mess = "Виникла помилка!"
     rnd_fact = randint(1, howmanyfacts)
 
     if rnd_fact == 1:
-        send_fact_mess = f"Данный бот является FOSS проектом. Это означает, что его <a href='github.com/KUKURUZKA165/roulette-telegram-bot'>исходный код</a> открыт всем желающим. Любой может проверить честность его работы :)"
+        send_fact_mess = f"Цей бот є FOSS проектом. Це означає, що його <a href='github.com/KUKURUZKA165/roulette-telegram-bot'>вихідний код</a> відкритий усім охочим. Будь-хто може перевірити чесність його роботи :)"
     else:
         if rnd_fact == 2:
-            send_fact_mess = f"Случайность в данном боте реализована через атмосферный шум (random.org). Соременные технологии не способны предугадать результат вашей игры."
+            send_fact_mess = f"Випадковість у цьому роботі реалізована через атмосферний шум (random.org). Сучасні технології не здатні передбачити результат вашої гри."
         else:
             if rnd_fact == 3:
-                send_fact_mess = f"Бот знает только {howmanyfacts} фактов о себе. Он просто перебирает факты случайным образом и отправяет вам."
+                send_fact_mess = f"Бот знає тільки {howmanyfacts} фактів себе. Він просто перебирає факти випадково і відправляє вам."
             else:
                 if rnd_fact == 4:
-                    send_fact_mess = f"Этот факт ещё не придумали."
+                    send_fact_mess = f"Цей факт ще не придумали. Я не знаю що тут написати("
                 else:
                     if rnd_fact == 5:
-                        send_fact_mess = f"Даже если бы все казино использовали такой же способ получения случайных чисел, как у нас, они бы всё равно вероятнее всего оставались в плюсе."
+                        send_fact_mess = f"Навіть якби всі казино використовували такий самий спосіб отримання випадкових чисел, як у нас, вони все одно залишалися б у плюсі."
 
-    await bot.send_message(message.chat.id, "<b>Интересный факт:</b>\n" + send_fact_mess, parse_mode="html", disable_web_page_preview=True)
+    await bot.send_message(message.chat.id, "<b>Цікавий факт:</b>\n" + send_fact_mess, parse_mode="html", disable_web_page_preview=True)
 
-    await log(f"Я отправил факт {rnd_fact} из {howmanyfacts}. Его содержание:\n{send_fact_mess}")
+    await log(f"Я надіслав факт {rnd_fact} з {howmanyfacts}. Його зміст:\n{send_fact_mess}")
 
 
 @dp.message_handler(commands=["help"])
 async def help_command(message):
     await log(f"--------------------\n{time.ctime()}\n{message.from_user.first_name} {message.from_user.last_name} @{message.from_user.username} id={message.from_user.id}\n{message.chat.title} {message.chat.invite_link} id = {message.chat.id}\n-")
-    await log("Запрошена помощь")
+    await log("Запитана допомога")
     send_mess = helpmsg
     await bot.send_message(message.chat.id, send_mess, parse_mode="html")
     await log("Готово")
@@ -215,23 +216,23 @@ async def help_command(message):
 async def mess(message):
     get_message_bot = message.text.strip().lower()
 
-    if get_message_bot == "играть":
+    if get_message_bot == "грати":
         await roll(message)
     else:
         if get_message_bot == "автор":
             await author(message)
         else:
-            if get_message_bot == "случайный факт":
+            if get_message_bot == "випадковий факт":
                 await fact(message)
             else:
                 await log(f"--------------------\n{time.ctime()}\n{message.from_user.first_name} {message.from_user.last_name} @{message.from_user.username} id={message.from_user.id}\n{message.chat.title} {message.chat.invite_link} id = {message.chat.id}\n-\nПрислал(а): {message.text}")
                 if message.from_user.id == message.chat.id:
-                    send_mess = f"<b>Я вас не понял :(</b>"
+                    send_mess = f"<b>Я вас не зрозумів :(</b>"
                     await bot.send_message(message.chat.id, send_mess, parse_mode="html")
-                    await log("Отправил пользователю, что я его не понял")
+                    await log("Надіслав користувачеві, що я його не зрозумів")
 
 
-print(f"Добро пожаловать!")
+print(f"Доброго дня, Слава Україні!")
 
 if __name__ == '__main__':
     executor.start_polling(dp)
